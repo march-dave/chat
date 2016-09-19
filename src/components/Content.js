758c03dd8a3c71b764541d3b3c35acacc7b86ca9
march-dave <ysangy@gmail.com>
2016-09-19 08:21:07 -0400

    add content

---
+++ src/components/Content.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Content extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Content</h2>
+        <p>Hello Content</p>
+      </div>
+    );
+  }
+}
+
+export default Content;
